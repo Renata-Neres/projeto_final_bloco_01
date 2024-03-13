56886503c69ee5eb24a9667590155d119caa3e61
Renata-Neres <cristina.neres23@gmail.com>
2024-03-13 14:40:23 -0300

    Menu

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,47 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
+		
+		int codigo;
+		String marca, nome, notas;
+		float  valor;
+		
+		while(true) {
+			
+		            System.out.println("*~*~*~*~*~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~*~*~");
+		            System.out.println("                                                     ");
+		            System.out.println("                                                     ");
+		            System.out.println("        -------------------------------------------  ");
+					System.out.println("         BOAS VINDAS A PERFUMARIA PARFUM BY NERES!   ");
+					System.out.println("        -------------------------------------------  ");
+					System.out.println("                                                     ");
+					System.out.println("                                                     ");
+					System.out.println("*~*~*~*~*~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~*~*~");
+					System.out.println("          Lojista selecione a opção desejada:        ");
+					System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+					System.out.println("                                                     ");
+					System.out.println("            1 - Adicione os produtos:                ");
+					System.out.println("            2 - Liste os produtos:                   ");
+					System.out.println("            3 - Busque os produtos por código:       ");
+					System.out.println("            4 - Atualize os dados do produtos:       ");
+					System.out.println("            5 - Apagar produto:                      ");
+					System.out.println("            9 - Sair                                 ");
+					System.out.println("                                                     ");
+					System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+					System.out.println("                                                     ");
+					
+					
+					
+				
+		}
+		
+	  }	
+   }
+
+		
